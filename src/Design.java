5509a40e848fa16ae6424fa6b46847e6f92d1a8f
ShashyCuber <shashwat.sparsh@gmail.com>
2017-08-15 10:10:16 -0700

    Initial Publish

---
+++ src/Design.java
@@ -0,0 +1,45 @@
+
+public class Design 
+{
+	public static void end()
+	{
+		System.out.println("(******)");
+	}
+	
+	public static void face()
+	{
+		System.out.println(" oo oo  ");
+		System.out.println("   ^    ");
+		System.out.println("  ~~~   ");
+	}
+	
+	public static void bracket()
+	{
+		System.out.println("<<<<>>>>");
+	}
+	
+	public static void doubleface()
+	{
+		end();
+		face();
+		bracket();
+		face();
+	}
+	
+	public static void middle()
+	{
+		bracket();
+		System.out.println("~!~!~!~!");
+		bracket();
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		doubleface();
+		middle();
+		doubleface();
+		end();
+	}
+	
+}
